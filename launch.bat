b13e2d5fc1dbdb90772434b6d8dde1e03a478dc5
Gabin Longoni <77763298+Opperiesen@users.noreply.github.com>
2023-03-22 12:01:56 +0100

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -2,4 +2,4 @@ set ffDecJar="C:\Program Files (x86)\FFDec\ffdec.jar"
 set dofInvoker="C:\Users\%USERNAME%\Desktop\DofusSources\DofusInvoker.swf"
 set pathDecompile="C:\Users\%USERNAME%\Desktop\DofusSources"
 
-java -jar "%ffDecJar%" -selectclass com.ankamagames.dofus.network.++,com.ankamagames.dofus.datacenter.++ -export script "%pathDecompile%" "%dofInvoker%"
+java -jar %ffDecJar% -selectclass com.ankamagames.dofus.network.++,com.ankamagames.dofus.datacenter.++ -export script %pathDecompile% %dofInvoker%
