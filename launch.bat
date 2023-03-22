ec9d27b8aeee610861fa2a2b0474d58543da195f
Gabin Longoni <77763298+Opperiesen@users.noreply.github.com>
2023-03-22 12:00:30 +0100

    Add files via upload

---
+++ launch.bat
@@ -0,0 +1,5 @@
+set ffDecJar="C:\Program Files (x86)\FFDec\ffdec.jar"
+set dofInvoker="C:\Users\%USERNAME%\Desktop\DofusSources\DofusInvoker.swf"
+set pathDecompile="C:\Users\%USERNAME%\Desktop\DofusSources"
+
+java -jar "%ffDecJar%" -selectclass com.ankamagames.dofus.network.++,com.ankamagames.dofus.datacenter.++ -export script "%pathDecompile%" "%dofInvoker%"
